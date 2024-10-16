f5ca6fd3bd65825f620f5adb287e9a057ea6418d
pereruannabaala <pereruannabaala@gmail.com>
2024-10-16 21:56:51 +0300

    add search query

--- app.py
+++ app.py
@@ -22,3 +22,34 @@ all_headlines = newsapi.get_top_headlines(country="in",
                                           language="en",
                                           page_size=total_results)['articles']
 
+# this function will return sources and domains in str format
+# each of them separated by a comma
+def get_source_and domains():
+    all_sources = newsapi.get_sources()['sources']
+    sources = []
+    domains = []
+    for e in all_sources:
+        id = e['id']
+        domain = e['url'].replace("https://", "")
+        domain = domain.replace("https://","")
+        domain = domain.replace("www.","")
+        slash = domain.find('/')
+        if slash != -1:
+            domain = domain[:slash]
+        sources.append(id)
+        domains.append(domain)
+    sources = ",".join(sources)
+    domains = ",".join(domains)
+    return sources,domains
+
+# in post request
+sources, domains = get_sources_and_domains()
+related_news = newsapi.get_everything(q=keyword,
+                                      sources=sources,
+                                      domains=domains,
+                                      language='en',
+                                      sort_by='relevancy')
+
+
+
+                                      
